692f1a9a3cc44d4cefbb729c9102abc4eb279986
lanhao <admin@lanhao.name>
2021-03-15 16:58:43 +0800

    feat: 正确识别 git-flow 带斜杠层次结构的 branch

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { ActionBase, ArgvOption, IWithSubCommands } from '@mohism/sloty';
 import { Dict } from '@mohism/utils';
-import { existsSync, readdirSync, readFileSync } from 'fs';
+import { existsSync, readdirSync, readFileSync, statSync } from 'fs';
 import { resolve } from 'path';
 
 class PruneBranchAction extends ActionBase {
@@ -12,13 +12,26 @@ class PruneBranchAction extends ActionBase {
     return `prune you git`;
   }
 
+  getBranchs(headsPath: string, prefix = ''): Array<string> {
+    const result: Array<string> = [];
+    const children = readdirSync(headsPath);
+    children.forEach(child => {
+      if (!statSync(`${headsPath}/${child}`).isDirectory()) {
+        result.push([prefix, child].filter(item => item).join('/'));
+      } else {
+        result.push(...this.getBranchs(`${headsPath}/${child}`,[prefix, child].filter(item => item).join('/')));
+      }
+    });
+    return result;
+  }
+
   async run(options: IWithSubCommands): Promise<void> {
     const d = resolve(options.subCommands[1] || '.');
     this.info(`目录：${d}`)
     if (!existsSync(`${d}/.git`)) {
       this.fatal('不是一个git仓库');
     }
-    const branchs = readdirSync(`${d}/.git/refs/heads`);
+    const branchs = this.getBranchs(`${d}/.git/refs/heads`);
     const toDelete = await this.question.checkbox('选择要清理的本地分支', branchs);
     if (!await this.question.confirm(`确认删除分支: ${toDelete.join(', ')}`)) {
       this.done('放弃操作');
